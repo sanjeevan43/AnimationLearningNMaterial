3d44eb2d7329c2e69c23d6d88995034a1cb9ea03
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-26 10:21:39 +0530

    Initial Commit

---
+++ AnimationLearningNMaterial/AnimationLearningNMaterialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationLearningNMaterialApp.swift
+//  AnimationLearningNMaterial
+//
+//  Created by sanjeevan on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationLearningNMaterialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
