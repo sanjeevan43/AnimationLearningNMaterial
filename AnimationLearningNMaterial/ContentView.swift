964636250b02ff8d07d7a12ba7fe1d516d19fd4f
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-26 11:23:19 +0530

    day night

--- AnimationLearningNMaterial/ContentView.swift
+++ AnimationLearningNMaterial/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    let sunGradient = LinearGradient(colors: [.red,.yellow,.orange], startPoint: .topLeading, endPoint: .bottomTrailing)
+    let moonGradient = LinearGradient(colors: [.clear,.clear,.yellow], startPoint: .topLeading, endPoint: .bottomTrailing)
+    let dayBackground = LinearGradient(colors: [.sky,.sky,.white], startPoint: .topLeading, endPoint: .bottomTrailing)
+    let nightBackground = LinearGradient(colors: [.black], startPoint: .topLeading, endPoint: .bottomTrailing)
+    @State private var isCloudsMove = false
+    @State private var isDay = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            isDay ? dayBackground.edgesIgnoringSafeArea(.all):nightBackground.edgesIgnoringSafeArea(.all)//declare sk
+            if !isDay{
+                ForEach(0..<100,id:\.self){_ in
+                    Circle()
+                        .fill(.white)
+                        .frame(width: CGFloat.random(in: 2...3), height: CGFloat.random(in: 2...3))
+                        .position(x:CGFloat.random(in: 0...500),y:CGFloat.random(in: 0...1000))
+                }
+            }
+            Circle().fill(sunGradient).frame(width: 100, height: 100)
+                .shadow(color:.red,radius: 10)
+                .offset(y:isDay ? -300 : 400)
+                
+            
+            Circle().fill(moonGradient).frame(width: 100, height: 100)
+                .shadow(color:.red,radius: 10)
+                .offset(y:isDay ? 400 : -300)
+            
+            Image("Clouds")
+                .resizable()
+                .frame(width: 500,height: 200)
+                .offset(x:isCloudsMove ? 500 : -500,y:-300)
+                .animation(.easeInOut(duration: 10).repeatForever(autoreverses: false), value: isCloudsMove)
+                .onAppear{
+                    isCloudsMove.toggle()
+                    
+                }
+            
+            Image("Sceneary")
+                .resizable()
+                .frame(width: 500,height: 400)
+                .offset(y:300)
         }
-        .padding()
+        .animation(.easeInOut(duration: 10).repeatForever(autoreverses: false), value: isDay)
     }
 }
 
